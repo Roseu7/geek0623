7b1e15d9f4e13822824701eea5a0e06ce0c2a3ba
nka21 <naoki.nico@gmail.com>
2024-06-17 03:48:15 +0900

    feat: 背景画像コンポーネントの作成
    フリー画像を使用

---
+++ src/components/BackgroundImage.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import bgImage from "@/assets/bg.png";
+
+export function BackgroundImage() {
+  return (
+    <div className="absolute top-0 left-0 w-full h-full -z-10 blur-sm">
+      <Image
+        src={bgImage}
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        priority={true}
+      />
+    </div>
+  );
+}
