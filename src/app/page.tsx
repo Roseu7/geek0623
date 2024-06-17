0ab5c876efb8d3172995941047be2365f7b6361c
nka21 <naoki.nico@gmail.com>
2024-06-17 13:17:56 +0900

    使うと予想されるコンポーネントファイルの作成
    mainページにhtmlの記述

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { AuthPage } from "@/features/auth/components/AuthPage";
+import { Main } from "@/features/main/components/Main";
 
 export default function Home() {
   return (
     <div className="relative antialiased">
-      <AuthPage />
+      {/* <AuthPage /> */}
+      <Main />
     </div>
   );
 }
