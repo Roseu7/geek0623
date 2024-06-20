fbe4eebad39cbf6147c8f93b0c4ba75a8042298c
nka21 <naoki.nico@gmail.com>
2024-06-20 22:30:52 +0900

    feat: Update Footer component layout
    フッターコンポーネントのレイアウトを更新しました

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div className="sticky bottom-0 z-10 bg-white shadow-sm">
-      <nav className="container mx-auto flex justify-between px-16 py-5">
+      <nav className="container mx-auto flex justify-between px-16 py-4">
         <a href="/home">
           <div>ホーム</div>
         </a>
