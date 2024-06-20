6f1747b99b9c11be11cfa924e4542679e82c040b
Roseu7 <keitoh1007@gmail.com>
2024-06-21 03:32:11 +0900

    Middlewareの再配置

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,6 +1,15 @@
 import { AuthPage } from "@/features/auth/components/AuthPage"
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Auth() {
+export default async function Auth() {
+
+    const supabase = createClient()
+    const { data } = await supabase.auth.getUser()
+
+    if (data?.user) {
+        redirect('/home')
+    }
     return (
         <div className="relative antialiased">
             <AuthPage />
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,12 @@ import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
 
 export async function middleware(request: NextRequest) {
+    console.log("Middleware triggered")
     return await updateSession(request)
 }
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        '/((?!_next/static|_next/image|favicon.ico|auth|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)'
     ],
 }
