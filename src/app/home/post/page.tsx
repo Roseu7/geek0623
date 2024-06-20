485c8d24aeee6303800cee015e05599c003c3e85
Roseu7 <keitoh1007@gmail.com>
2024-06-21 01:00:03 +0900

    修正

--- src/app/home/post/page.tsx
+++ src/app/home/post/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -9,10 +8,7 @@ import DairyInput from "@/features/main/components/DairyInput";
 export default function page() {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-100">
-      {/* useLocationフックを使用するには、<Router>コンポーネントでラップしなければならない */}
-      <Router>
-        <Header />
-      </Router>
+      <Header />
       <main className="flex-1 overflow-y-auto">
         <div className="container mx-auto px-4 py-6">
           <DairyInput />
