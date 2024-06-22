25ef689dd543657bc4a32b6a9b0f94128805e731
nka21 <naoki.nico@gmail.com>
2024-06-23 06:20:01 +0900

    chore: Update Footer component with IconLink for improved code readability

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,33 +4,22 @@ import { MdHome, MdPerson } from "react-icons/md";
 import { HiArchive } from "react-icons/hi";
 import React from "react";
 
+// アイコンリンクコンポーネントの定義
+const IconLink = ({ href, icon }) => (
+  <Link href={href}>
+    <Icon as={icon} height={8} width={8} />
+  </Link>
+);
+
 export default function Footer() {
   return (
-    // <div className="sticky bottom-0 z-10 bg-white shadow-sm">
-    //   <nav className="container mx-auto flex justify-between px-16 py-4">
-    //     <a href="/home">
-    //       <div>ホーム</div>
-    //     </a>
-    //     <a href="/archive">
-    //       <div>アーカイブ</div>
-    //     </a>
-    //     <a href="/mypage">
-    //       <div>マイページ</div>
-    //     </a>
-    //   </nav>
-    // </div>
     <Box position="sticky" bottom={0} zIndex={10} bg="white" boxShadow="sm">
       <Container maxW="container.lg" px={16} py={3}>
         <Flex justify="space-between">
-          <Link href="/home">
-            <Icon as={MdHome} height={8} width={8} />
-          </Link>
-          <Link href="/archive">
-            <Icon as={HiArchive} height={8} width={8} />
-          </Link>
-          <Link href="/mypage">
-            <Icon as={MdPerson} height={8} width={8} />
-          </Link>
+          {/* アイコンリンクの使用 */}
+          <IconLink href="/home" icon={MdHome} />
+          <IconLink href="/archive" icon={HiArchive} />
+          <IconLink href="/mypage" icon={MdPerson} />
         </Flex>
       </Container>
     </Box>
