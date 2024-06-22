9a176f02b2eb2e46a4c5d957a50a011cd88ae069
nka21 <naoki.nico@gmail.com>
2024-06-23 06:19:55 +0900

    chore: Update Header component with localized titles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,49 +6,30 @@ import { SettingsIcon } from "@chakra-ui/icons";
 import { genjyuuBold } from "@/assets/fonts/fonts";
 
 export default function Header() {
-  // useLocationから現在のURLのパスを取得
   const pathname = usePathname() || "/";
 
-  // ヘッダータイトルを格納する変数を初期化
-  let title: string = "";
+  // パスに応じたヘッダータイトルのマッピング
+  const titles = {
+    "/home": "ホーム",
+    "/archive": "アーカイブ",
+    "/mypage": "マイページ",
+    "/settings": "設定",
+  };
 
-  // パスに応じてヘッダータイトルを設定
-  switch (pathname) {
-    case "/home":
-      title = "ホーム";
-      break;
-    case "/archive":
-      title = "アーカイブ";
-      break;
-    case "/mypage":
-      title = "マイページ";
-      break;
-    case "/settings":
-      title = "設定";
-      break;
-  }
+  // 現在のパスに基づいてタイトルを取得
+  const title = titles[pathname] || "";
 
   return (
-    // <header className="bg-white shadow-sm sticky top-0 z-10">
-    //   <div className="container mx-auto px-6 py-3 flex items-center justify-between">
-    //     <a href={pathname}>{title}</a>
-    //     <a href="/settings">
-    //       <div>設定</div>
-    //     </a>
-    //   </div>
-    // </header>
     <Box bg="white" boxShadow="sm" position="sticky" top={0} zIndex={10}>
       <Container maxW="container.lg" px={6} py={3}>
         <Flex align="center" justify="space-between">
           <Link href={pathname}>
-            {/* <Box >{title}</Box> */}
             <Heading as="h1" size="md" fontFamily={genjyuuBold.className}>
               {title}
             </Heading>
           </Link>
           <Link href="/settings">
-            {/* <Box>設定</Box> */}
-            <SettingsIcon boxSize={6}></SettingsIcon>
+            <SettingsIcon boxSize={6} />
           </Link>
         </Flex>
       </Container>
