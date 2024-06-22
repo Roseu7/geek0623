9c38b22f64dd2acc21bfc186aba0c06c370f36ba
nka21 <naoki.nico@gmail.com>
2024-06-23 06:20:09 +0900

    chore: Refactor MessageInput component and add user info retrieval and message sending functionality

--- src/features/main/components/MessageInput.tsx
+++ src/features/main/components/MessageInput.tsx
@@ -9,86 +9,70 @@ export default function MessageInput(): JSX.Element {
   const [text, setText] = useState("");
   const [user, setUser] = useState<string | undefined>(undefined);
 
+  // ユーザー情報の取得
   useEffect(() => {
-    (async () => {
+    const fetchUser = async () => {
       const { data } = await supabase.auth.getUser();
       setUser(data.user?.id);
-    })();
+    };
+    fetchUser();
   }, []);
 
+  // メッセージの送信
   const sendMessage = async () => {
-    try {
-      if (!(text === "" || text === null || text === undefined)) {
-        const { error } = await supabase.from("messages").insert([
-          {
-            text: text.trim(),
-            uid: user,
-            created_at: new Date().toISOString(),
-          },
-        ]);
+    if (!text.trim()) {
+      console.error("Message is empty");
+      return;
+    }
 
-        if (error) {
-          throw error;
-        }
-      } else {
-        console.error("Message is empty");
-      }
+    try {
+      const { error } = await supabase.from("messages").insert([
+        {
+          text: text.trim(),
+          uid: user,
+          created_at: new Date().toISOString(),
+        },
+      ]);
+
+      if (error) throw error;
 
       setText("");
-
-      const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
-      if (isMobile) {
-        window.location.href = "/home";
-      } else {
-        window.location.reload();
-      }
+      navigateAfterSend();
     } catch (error) {
       console.error("Error sending message:", error);
     }
   };
 
+  // 送信後のナビゲーション
+  const navigateAfterSend = () => {
+    const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      window.location.href = "/home";
+    } else {
+      window.location.reload();
+    }
+  };
+
   return (
-    // <Box pos="relative">
-    //   <Textarea
-    //     value={text}
-    //     placeholder="今日はどんなことがありましたか？"
-    //     resize={"none"}
-    //     colorScheme="gray"
-    //     variant={"filled"}
-    //     as={AutosizeTextarea}
-    //     bg={"#FFFFFF"}
-    //     onChange={(e) => setText(e.target.value)}
-    //   />
-    //   <Box>
-    //     <Button
-    //         colorScheme="teal"
-    //         pos={"absolute"}
-    //         right="0%"
-    //         onClick={sendMessage}
-    //       >
-    //         投稿する
-    //     </Button>
-    //   </Box>
-    // </Box>
-    <Card variant={"outline"} padding={2} borderRadius={"lg"}>
-      <Flex direction={"column"}>
+    <Card variant="outline" padding={2} borderRadius="lg">
+      <Flex direction="column">
         <Textarea
           value={text}
           placeholder="今日のできごとを瓶に入れて誰かに届けてみましょう！"
-          resize={"none"}
-          variant={"filled"}
+          resize="none"
+          variant="filled"
           as={AutosizeTextarea}
-          bg={"#FFFFFF"}
+          bg="#FFFFFF"
           onChange={(e) => setText(e.target.value)}
           _hover={{ bg: "#FFFFFF", borderColor: "gray.300" }}
         />
-        <Box my={2} w={"full"}>
+        <Box my={2} w="full">
           <Divider />
         </Box>
         <Button
-          colorScheme={"teal"}
-          variant={"outline"}
-          alignSelf={"flex-end"}
+          colorScheme="teal"
+          variant="outline"
+          alignSelf="flex-end"
           onClick={sendMessage}
         >
           ボトメる
