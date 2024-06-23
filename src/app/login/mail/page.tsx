bd14c82ce1ee3513ef727be07d250f353545b072
nka21 <naoki.nico@gmail.com>
2024-06-23 12:48:56 +0900

    認証コードのフロント

--- src/app/login/mail/page.tsx
+++ src/app/login/mail/page.tsx
@@ -1,6 +1,47 @@
 import BackArrow from "@/components/ui/BackArrow";
+import {
+  Box,
+  Button,
+  HStack,
+  Heading,
+  PinInput,
+  PinInputField,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function page() {
-  return <BackArrow />;
+  return (
+    <>
+      <BackArrow />
+      <Box marginX={8} marginTop={8}>
+        <Stack spacing={2}>
+          <Heading as="h1" size={"lg"}>
+            認証コード
+          </Heading>
+          <Text marginBottom={4}>naoki.nico@gmail.comへ送信しました</Text>
+          <HStack align={"center"} gap={2} marginBottom={6}>
+            <PinInput type="alphanumeric" size="lg">
+              {/* 何桁かは任せる */}
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+          <Button
+            colorScheme="teal"
+            borderRadius={"full"}
+            paddingY={6}
+            paddingX={8}
+          >
+            再送信する
+          </Button>
+        </Stack>
+      </Box>
+    </>
+  );
 }
